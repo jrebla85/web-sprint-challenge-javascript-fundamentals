718009b25d7e56f60766b74efdbd5d74dae2f175
jeremy black <jrebla85@yahoo.com>
2021-01-22 21:18:19 -0600

    Completed Topic 1

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
+// The nestedFunction is of the same scope as internal
 
 
 
@@ -28,9 +28,9 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num) {
+    let res = (num * (num + 1)) / 2;
+    return res
   }
  
 
